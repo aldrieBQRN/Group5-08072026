d5f244ec0b6142950569b287650d15f2087c4a26
John Aldrie BAquiran <aldriebaquiran15@gmail.com>
2026-07-08 10:05:59 +0800

    Add email

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul>
-            <li><a href="#">Gmail</a></li>
+            <li><a href="aldriebaquiran15@gmail.com">Gmail</a></li>
             <li><a href="#">Images</a></li>
         </ul>
     </nav>
