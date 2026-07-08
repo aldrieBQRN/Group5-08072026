457345da0d5f21d3434d895eced8f2a4685b9a3d
John-Carlo-Brcelon <23-77394@g.batstate-u.edu.ph>
2026-07-08 10:38:58 +0800

    footer functionality

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+
+<main>
+    <div class="search-container">
+        <input type="text" placeholder="Search Google or type a URL">
+    </div>
+</main>
+
+<?php include 'footer.php'; ?>
